f1168948a24cac99ba2c30318b6501590007ca82
Tave23 <tavella01@gmail.com>
2021-10-20 15:41:35 +0200

    generatore password poco sicuro

---
+++ js/script.js
@@ -0,0 +1,29 @@
+/* Chiedi all’utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+Infine scrivi sulla pagina nomecognomecolorepreferito21 */
+
+
+const firstname = prompt ('Qual è il tuo nome?')
+const surname = prompt('Qual è il tuo cognome?');
+const prefColor = prompt('Qual è il tuo colore preferito?');
+
+document.getElementById("Passwordgenerata").innerHTML =
+`
+   <h1>
+      Password generata : ${firstname}${surname}${prefColor}21
+   </h1>
+`;
+
+
+console.log(firstname);
+console.log(surname);
+console.log(prefColor);
+
+
+
+
+
+
+
+
